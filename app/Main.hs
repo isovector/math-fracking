7494d93818ac8a39c9daefdd900655e6d315ce63
Sandy Maguire <sandy@sandymaguire.me>
2018-11-21 01:46:21 -0500

    FRACK THAT MATH BABY

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,29 @@
 module Main where
 
+import Data.Foldable
 import Lib
+import qualified Data.Text.IO as TIO
+import System.Environment
+import Text.LaTeX.Base.Parser
+import Text.LaTeX.Base.Render
+
+
+parserConf :: ParserConf
+parserConf = ParserConf
+  { verbatimEnvironments = ["code", "verbatim", "lstlisting"]
+  }
+
 
 main :: IO ()
-main = someFunc
+main = do
+  [prefix, file] <- getArgs
+  Right latex <- parseLaTeXFileWith parserConf file
+  let (result, jobs) = frack prefix latex
+
+  for_ (zip [0..] jobs) $ \(i, job) ->
+    TIO.writeFile (prefix ++ show i ++ ".tex")
+      . render
+      $ getMathJob job
+
+  TIO.putStrLn $ render result
+
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,63 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications  #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+{-# OPTIONS_GHC -Wall #-}
+
+module Lib where
+
+import Control.Applicative
+import Control.Monad.State
+import Data.Text (pack)
+import Text.LaTeX
+import Text.LaTeX.Base.Syntax
+
+type Latex = LaTeX
+
+
+newtype MathJob = MathJob { getMathJob :: Latex }
+
+
+extractMath :: Latex -> [MathJob]
+extractMath (TeXRaw _)         = []
+extractMath (TeXComm _ ns)     = extractMath =<< extractArgs =<< ns
+extractMath (TeXCommS _)       = []
+extractMath (TeXEnv _ ns t)    = extractMath =<< ((++ [t]) . extractArgs) =<< ns
+extractMath t@(TeXMath _ _)    = pure $ MathJob t
+extractMath (TeXLineBreak _ _) = []
+extractMath (TeXBraces t)      = extractMath t
+extractMath (TeXComment _)     = []
+extractMath (TeXSeq a b)       = extractMath =<< [a, b]
+extractMath (TeXEmpty)         = []
+
+extractArgs :: TeXArg -> [Latex]
+extractArgs (FixArg t)  = pure t
+extractArgs (OptArg t)  = pure t
+extractArgs (MOptArg t) = t
+extractArgs (SymArg t)  = pure t
+extractArgs (MSymArg t) = t
+extractArgs (ParArg t)  = pure t
+extractArgs (MParArg t) = t
+
+
+blowoutMath :: String -> Latex -> Latex
+blowoutMath prefix =
+    fst . flip runState (0 :: Int) . texmapM isMath makeInput
+  where
+    isMath (TeXMath _ _) = True
+    isMath _             = False
+
+    makeInput = const $ do
+      v <- get
+      modify (+1)
+      pure . TeXComm "input"
+           . pure
+           . FixArg
+           . TeXRaw
+           . pack
+           $ prefix ++ show v
+
+
+frack :: String -> Latex -> (Latex, [MathJob])
+frack prefix = liftA2 (,) (blowoutMath prefix) extractMath
+
